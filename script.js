309ddb1937ceed66808defe2f3b066c153506eb2
manueledigregorio <dig.manuele@gmail.com>
2023-10-07 11:57:55 +0200

    [add] getApi()

--- script.js
+++ script.js
@@ -2,4 +2,38 @@ const { createApp }= Vue;
 
 createApp({
 
-}).mounth('#id')
+  data(){
+    return{
+      urlApi:'https://flynn.boolean.careers/exercises/api/random/mail',
+      emails:[]
+
+    }
+    
+  },
+  mounted() {
+   this.getApi()
+  },
+
+  methods: {
+
+    getApi(){
+      for (let i = 0; i < 10; i++) {
+        axios.get(this.urlApi)
+
+         .then((response) => {
+            this.emails.push(response.data.response)
+            console.log(this.emails)
+         })
+
+         .catch((error) => {
+          console.log(error)
+        })
+        
+      }
+    }
+    
+  },
+
+
+
+}).mount('#app')
